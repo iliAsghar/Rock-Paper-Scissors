6245973d479c0cb96a8ed0cbc22969bbaf90c18d
iliAsghar <asghar.ali.z.a.r.e@gmail.com>
2024-07-11 01:29:15 +0330

    finished the game run function in js

---
+++ assets/scripts/rock-paper-scissors.js
@@ -0,0 +1,74 @@
+const allMoves = [
+  { move:'rock' , weakTo: 'paper', strongTo:'scissors'},
+  { move:'paper', weakTo: 'scissors', strongTo:'rock'},
+  { move:'scissors' , weakTo: 'rock', strongTo:'paper'}
+]
+
+let totalPoints = {
+  userPoints: 0,
+  computerPoints: 0
+};
+
+export function runRPS(playerMoveName){
+  let playerMove = allMoves.find(moveObj => {return moveObj.move === playerMoveName});
+  if(!playerMove){
+    alert('Invalid Player Move!');
+    return null;
+  }
+  let computerMove = getComputerMove();
+  let points = getRoundPoint(playerMove, computerMove);
+  updateTotalPoints(points);
+  setTimeout(() => {
+    initiateComputerMove(computerMove.move); 
+    setTimeout(() => {
+      updateScoreBoard();
+      resetChoices();
+    }, 1000);
+  }, Math.floor(Math.random() * 4000) + 750);
+
+}
+
+function getComputerMove(){
+  let index = Math.floor((Math.random()) * 3);
+  return allMoves[index];
+}
+
+function getRoundPoint(pMove, eMove){
+  if(pMove.move === eMove.weakTo){
+    return [1, 0];
+  } else if(pMove.move === eMove.strongTo){
+    return [0, 1];
+  } else {
+    return [0, 0];
+  }
+} 
+
+function updateTotalPoints(newPoints){
+  totalPoints.userPoints += newPoints[0];
+  totalPoints.computerPoints += newPoints[1];
+}
+
+const userScoreElem = document.querySelector('.player-score');
+const computerScoreElem = document.querySelector('.computer-score');
+
+function updateScoreBoard(){
+  userScoreElem.innerHTML = totalPoints.userPoints;
+  computerScoreElem.innerHTML = totalPoints.computerPoints;
+}
+
+
+const computerMoveElements = {
+  "rock": document.querySelector('.computer-rock'),
+  "paper": document.querySelector('.computer-paper'),
+  "scissors": document.querySelector('.computer-scissors')
+}
+
+function initiateComputerMove(computerMove){
+  computerMoveElements[computerMove].classList.toggle('active');
+}
+
+function resetChoices() {
+  document.querySelectorAll('.active').forEach(element => {
+    element.classList.remove('active')
+  })
+}
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,28 @@
 "use strict";
+import { runRPS } from "./rock-paper-scissors.js";
 
 const startBtn = document.querySelector('.board__start-btn');
 
 startBtn.addEventListener('click',() => {
   window.scroll(0, window.innerHeight)
+})
+
+const playerBtns = document.querySelectorAll('.game__choice-btn--player');
+
+playerBtns.forEach(element => {
+  element.addEventListener('click',() => {
+    element.classList.add('active');
+    runRPS(element.dataset.move);
+  })
+})
+
+document.querySelectorAll('.emoji').forEach(emoji => {
+  emoji.addEventListener('click',() => {
+    playerBtns.forEach((playerBtn) => {
+      if(playerBtn.dataset.move === emoji.dataset.move){
+        playerBtn.classList.add('active');
+        runRPS(playerBtn.dataset.move);
+      }
+    })
+  })
 })
